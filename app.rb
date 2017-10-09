baba60d00042f8d7af9ccf4807b03d1eeb653260
mattcrowe5 <crowe.matthew5@gmail.com>
2017-10-09 09:39:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Matt"
+  end
+
+  get '/hometown' do
+    "My hometown is Mendham"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Wet Sand"
+  end
+
 end
